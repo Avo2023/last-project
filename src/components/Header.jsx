55075d9c3081a044ebe24a85b040ad2bfefb8aa8
Avo <sankrosika1990@gmail.com>
2023-02-19 20:55:58 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React  from 'react';
+const Header = () =>{
+   return <header className='header'>
+    <img src='https://www.shutterstock.com/image-vector/mount-ararat-biblical-mountain-vector-260nw-2125577228.jpg'/>
+</header>}
+export default Header;
